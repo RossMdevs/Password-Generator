13906773ab54ff768e530c14917aeda4af32bd85
Ross M <37253938+RossMdevs@users.noreply.github.com>
2018-10-28 15:32:07 -0400

    Added some lines and additional commentary!

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+// Big Thanks to Pyxel for this code below!
+// Check him out on https://github.com/pyxelx
+
 const express = require('express')
 const app = express()
+
+//Change port app listens on
 const port = 3000
 
 app.use(express.static(__dirname));
@@ -8,6 +13,7 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "index.html");
 });
 console.log("---------------------")
+console.info()
 console.log("Secure Password Generator 1.0 | Created by Ross & Intel ")
-app.listen(port, () => console.log(`Listening on http://localhost:${port}!`))
-
+app.listen(port, () => console.log(`Listening on ${port}!`))
+console.log("Press CONTROL + C to terminate session!")
