778dd33dbe9d2032d787407ab5b4006b2dfa3ebb
Ross M <37253938+RossMdevs@users.noreply.github.com>
2018-10-29 21:06:23 -0400

    Added ability to Do stuffs
    You can now do stuffs (AKA) install node modules from this prompt

--- launcher.bat
+++ launcher.bat
@@ -4,8 +4,9 @@ color 4
 cls
 Echo Password Generator Launcher V1.0
 Echo ============================================================================
-Echo 1 Launch Generator            Launches Gen on [PORT Specified]
-Echo 2 Exit Launcher/Close         Closes this menu
+Echo 1 Install  Modules            Installs Required modules to load app!  (AFTER INSTALL RELAUNCH APP)          
+Echo 2 Launch Generator            Launches Gen on [PORT Specified]
+Echo 3 Exit Launcher/Close         Closes this menu
 Echo ============================================================================
 Choice /C 12 /M "Select a choice below"
 
@@ -14,10 +15,15 @@ If Errorlevel 1 Goto 1
 
 Goto End
 
+:3
+:End
+
 :2
-Goto End
+node main.js
+pause
+:End
 
 :1
-node main.js
+npm i
 pause
 :End
